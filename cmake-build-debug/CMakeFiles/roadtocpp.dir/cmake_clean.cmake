de1db4a9add9984d516592457057e1ff82b12e01
liudun <liudun@drilver.com>
2018-05-11 11:41:03 +0800

    fix

---
+++ cmake-build-debug/CMakeFiles/roadtocpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/roadtocpp.dir/main.cpp.obj"
+  "roadtocpp.pdb"
+  "roadtocpp.exe"
+  "roadtocpp.exe.manifest"
+  "libroadtocpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/roadtocpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/roadtocpp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for roadtocpp.
+# This may be replaced when dependencies are built.

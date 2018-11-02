98256544ff64555a5073c2485373123cae3d62f0
liudun <liudun@youzan.com>
2018-11-03 01:00:44 +0800

    fix

---
+++ first.c
@@ -0,0 +1,12 @@
+//
+// Created by soapsnake@gmail.com on 2018/11/3.
+//
+#include <stdio.h>
+int main(void) {
+    int num;
+    num = 1;
+    printf("i am a simple. \n");
+    printf("computer.\n");
+    printf("my favorite numser is %d beacuse it is first.\n", num);
+    return 0;
+}

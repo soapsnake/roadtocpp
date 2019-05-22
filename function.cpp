7e582efd6428affd1202c339e8fb5cfe3c8cc58a
liudun <liudun@youzan.com>
2019-05-22 23:38:04 +0800

    fix

--- function.cpp
+++ function.cpp
@@ -3,6 +3,7 @@
 //
 #include <iostream>
 #include <cmath>
+#include <array>
 #include "function.h"
 
 void arr_test(const int arr[]);  //定义函数的地方必须和实际函数完全一致,这里有cosnt,下边也一定得有
@@ -11,8 +12,10 @@ int sum_arr(const int *begin, const int *end);  //参数为数组的开始和结
 
 int c_in_str(const char *str);  //c风格的字符串,其实就是一个字符数组
 
-char * build_str(char c, int n);
+char * build_str(char c, int n); //返回指针的函数
 
+void swap(int & a, int & b);  //以引用作为形参的函数
+void swapoint(int * a, int * b);  //指针版交换变量函数
 
 struct travel_time {  //结构体与对象最大的不同,就是调用函数时传递的是结构体的一份副本,而不像对象那样传递的是引用
     int hours;
@@ -23,7 +26,15 @@ travel_time deal_travel(travel_time t1, travel_time t2);  //传递结构体
 //polar结构体的定义位于function.h中
 void deal_polar(const polar * p,  polar * pda);   //传递结构体的地址指针
 
+//内联函数了??????todo
+
+const int seasons = 4;
+const std::array<std::string, seasons> snames = {"Spring", "Summber", "Fall", "Winter"};
+
+void fill(std::array<double, seasons> * pa);  //这句写在using前,就必须加std::了,否则编译报错
 using namespace std;
+void fill(array<double, seasons> * pa);  //写在using后就不用写std::了,这很现实
+
 int main() {
 
     int arr[3] = {2,2,3};   //c++ 初始化数组的方法,不能像java一样写int[], arr[3]定义数组长度而不是取值
@@ -53,11 +64,11 @@ int main() {
     double angle = 0.32131;
     double range = 0.3232;
     polar polar1 = {angle, range};
-//    polar res3;
-//    cout << "test polar &address" << endl;
-//    deal_polar(&polar1, &res3);
-//    cout << "res3.distance: " << res3.distance << endl;
-//    cout << "res3.angle: " << res3.angle << endl;
+    polar res3;
+    cout << "test polar &address" << endl;
+    deal_polar(&polar1, &res3);
+    cout << "res3.distance: " << res3.distance << endl;
+    cout << "res3.angle: " << res3.angle << endl;
 
 
     polar * ppointer; //实验发现单独只声明一个指针而不进行初始化是没有办法使用的
@@ -78,6 +89,19 @@ int main() {
     cout << "*strpt = " << *strpt << endl;
 
 
+    int rats = 101;
+    int & rodents = rats;  //rodents 现在是指向rats的引用,c++中的引用可以指向任意变量,无论是否对象
+    cout << "rodents = " << rodents << endl;
+    rodents = 120;  //通过rodents改变了变量rats的值
+    cout << "after rodents change rats = " << rats << endl;
+
+    int a = 10;
+    int b = 15;
+    cout << "before swap, a = " << a << " b = " << b << endl;
+    swap(a, b);
+    cout << "after swap, a = " << a << " b = " << b << endl;
+    swapoint(&a, &b);   //指针作为形参的函数,传值的时候必须传递变量地址
+    cout << "after pointer swap, a = " << a << " b = " << b << endl;
 }
 
 void arr_test(const int arr[]) {
@@ -122,3 +146,20 @@ void deal_polar(const polar * p, polar * pda) {
     pda->distance = sqrt(p->distance);
 }
 
+void fill(std::array<double, seasons> * pa) {  //std:array<数组类型,数组长度>
+    for (int i = 0; i < pa->size(); ++i) {
+        
+    }
+}
+
+void swap(int & a, int & b) {
+    int temp = a;   //temp = 10
+    a = b;   //a = 15
+    b = temp;   //b = 10
+}
+
+void swapoint(int * a, int * b) {
+    int temp = *a;   //*a才能拿到变量a的值,因为现在传进来的a是一个内存地址
+    *a = *b;
+    *b = temp;
+}
---
+++ question166.cpp
@@ -0,0 +1,61 @@
+//
+// Created by 刘盾 on 2019-05-20.
+//
+
+#include <unordered_map>
+#include "iostream"
+using namespace std;
+
+class Solution {
+public:
+    string fractionToDecimal(int numerator, int denominator) {
+        // zero numerator
+        if (numerator == 0) return "0";
+
+        string res;
+        // determine the sign
+        if (numerator < 0 ^ denominator < 0) res += '-';
+
+        // remove sign of operands
+        numerator = abs(numerator), denominator = abs(denominator);
+
+        // append integral part
+        res += to_string(numerator / denominator);
+
+        // in case no fractional part
+        if (numerator % denominator == 0) return res;
+
+        res += '.';
+
+        unordered_map<int, int> map;
+
+        // simulate the division process
+        for (int64_t r = numerator % denominator; r; r %= denominator) {
+
+            // meet a known remainder
+            // so we reach the end of the repeating part
+            if (map.count(r) > 0) {
+                res.insert(map[r], 1, '(');
+                res += ')';
+                break;
+            }
+
+            // the remainder is first seen
+            // remember the current position for it
+            map[r] = res.size();
+
+            r *= 10;
+
+            // append the quotient digit
+            res += to_string(r / denominator);
+        }
+
+        return res;
+    }
+};
+
+int main() {
+    Solution solution;
+    cout << "here" << solution.fractionToDecimal(1, 2) << endl;
+
+}

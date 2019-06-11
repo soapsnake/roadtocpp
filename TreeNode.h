51d304e62c3091a6e0073534d25940584e1606e8
liudun <liudun@youzan.com>
2019-06-11 10:26:12 +0800

    fix

---
+++ TreeNode.h
@@ -0,0 +1,18 @@
+//
+// Created by 刘盾 on 2019-06-10.
+//
+
+
+#include <cstdio>
+
+#ifndef ROADTOCPP_TREENODE_H
+#define ROADTOCPP_TREENODE_H
+
+#endif //ROADTOCPP_TREENODE_H
+
+struct TreeNode {
+         int val;
+         TreeNode *left;
+         TreeNode *right;
+         TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
---
+++ question230.cpp
@@ -0,0 +1,34 @@
+//
+// Created by 刘盾 on 2019-06-10.
+//
+
+#include "TreeNode.h"
+#include "stack"
+
+using namespace std;
+
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        stack<TreeNode *> stack;  //指针泛型
+        TreeNode *p = root;
+        while(p || !stack.empty())
+        {
+            while(p)
+            {
+                stack.push(p);
+                p = p->left;
+            }
+            p = stack.top();
+            if(--k == 0)
+                return p->val;
+            stack.pop();
+            p = p->right;
+        }
+        return 0;  //必须要有返回值
+    }
+};
+
+int main() {
+
+}

245f00839e568b6f998ca0b5f9beb563d1ac6134
soapsnake <soapsnake@gmail.com>
2020-09-10 00:27:16 +0800

    fix

--- question1022.cpp
+++ question1022.cpp
@@ -8,13 +8,6 @@ using namespace std;
 class Solution {
 public:
     int sumRootToLeaf(TreeNode *root) {
-       return helper(root, 0);
-    }
-
-    int helper(TreeNode *root, int val) {
-        if (!root) return 0;
-        val = (val * 2 + root->val);
-        return root->left == root->right ? val : helper(root->left, val) + helper(root->right, val);
-    }
+      o
 };
 

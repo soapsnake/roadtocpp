91366e7b594e7349d0d56410ce58bc9e9c569b50
soapsnake <soapsnake@gmail.com>
2020-09-24 00:28:40 +0800

    fix

---
+++ questiongasstation.cpp
@@ -0,0 +1,26 @@
+//
+// Created by soapsnake@gmail.com on 24/9/20.
+//
+#include "vector"
+
+using  namespace std;
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int start = gas.size()-1;
+        int end = 0;
+        int sum = gas[start] - cost[start];
+        while (start > end) {
+            if (sum >= 0) {
+                sum += gas[end] - cost[end];
+                ++end;
+            }
+            else {
+                --start;
+                sum += gas[start] - cost[start];
+            }
+        }
+        return sum >= 0 ? start : -1;
+    }
+};

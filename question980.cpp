36b1a0350fa2110b18760565c243a08030f7b703
soapsnake <soapsnake@gmail.com>
2020-09-24 00:06:32 +0800

    fix

--- question980.cpp
+++ question980.cpp
@@ -35,9 +35,7 @@ public:
     }
 
     int uniquePathsIII(vector<vector<int>> &grid) {
-        int i_start = 0;
-        int j_start = 0;
-        int total_step = 0;
+        auto i_start = 0,j_start = 0,total_step = 0;
         for (int i = 0; i < grid.size(); ++i) {
             for (int j = 0; j < grid[0].size(); ++j) {
                 if (grid[i][j] == 1) {

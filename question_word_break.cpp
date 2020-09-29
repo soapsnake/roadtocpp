263e9b4b222d7cc625fb2b60c72953287c2bfb14
liudun <liudun@kuaishou.com>
2020-09-29 21:07:07 +0800

    fix

---
+++ question_word_break.cpp
@@ -0,0 +1,27 @@
+#include "string"
+#include "vector"
+using namespace std;
+
+/**
+ * Example 1:
+Input: s = "leetcode", wordDict = ["leet", "code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+Example 2:
+Input: s = "applepenapple", wordDict = ["apple", "pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+             Note that you are allowed to reuse a dictionary word.
+
+Example 3:
+Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+Output: false
+ * */
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        
+
+    }
+};

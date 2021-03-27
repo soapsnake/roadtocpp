2ce21e7e638083d45bebf0d62fc2ae6bb6787c6b
liudun <soapsnake@gmail.com>
2021-03-27 15:02:55 +0800

    fix

---
+++ convert_list_biner_value.cpp
@@ -0,0 +1,17 @@
+#include "ListNode.h"
+
+//
+// Created by soapsnake@gmail.com on 1/11/20.
+//
+class Solution {
+public:
+    int getDecimalValue(ListNode *head) {
+        int ret = 0;
+        while (head) {
+            ret <<= 1;
+            ret |= head->val;
+            head = head->next;
+        }
+        return ret;
+    }
+};

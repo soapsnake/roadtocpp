39b5f40dc88b5cf99af149e34f2b2a3ea077b2fc
liudun <liudun@kuaishou.com>
2021-04-18 10:23:42 +0800

    fix

---
+++ question_fibonacci.cpp
@@ -0,0 +1,21 @@
+//
+// Created by soapsnake@gmail.com on 4/16/21.
+//
+
+class Solution {
+ public:
+  int fib(int n) {
+    int first = 0;
+    int second = 1;
+    if (n == 1) {
+      return 0;
+    }
+
+    while (n-- > 1) {
+      int third = first + second;
+      first = second;
+      second = third;
+    }
+    return second;
+  }
+};
